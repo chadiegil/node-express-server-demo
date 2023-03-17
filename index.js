ec568b928def1428657022a6e2500312b615db51
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-03-17 23:45:20 +0800

    update from local

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-<<<<<<< HEAD
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world update!!! from local");
+});
+
+app.post("/create-user", (req, res) => {
+  const { username, password } = req.body; // Assuming incoming data is JSON
+
+  console.log(`${password} ${username}`);
+
+  res.status(200).send(`Hello ${username}!`);
 });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-=======
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world update!!!");
-});
-
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
->>>>>>> 47e655157d2313474e09b7423ef89fb71b379d82
