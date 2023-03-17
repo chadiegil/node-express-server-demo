825cfa70b33323c030a72f144fb4659f58d83ed4
chadiegil@gmail.com <chadiegil@gmail.com>
2023-03-17 11:46:18 -0400

    update from cpanel

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
-=======
 const express = require("express");
 const app = express();
 
@@ -20,4 +9,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
->>>>>>> 47e655157d2313474e09b7423ef89fb71b379d82
