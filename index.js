88fdd3d65eae163144dfdf0d18bfcfccd9f8865a
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-03-17 23:23:32 +0800

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world update");
+  res.send("Hello world update!!!");
 });
 
 app.listen(3000, () => {
